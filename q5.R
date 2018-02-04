d39c508b6572865cdc86b5755b67d70b9f91855f
Michael Levy <michael.levy@healthcatalyst.com>
2018-02-04 15:13:08 -0700

    in a function

--- q5.R
+++ q5.R
@@ -1,13 +1,17 @@
-steps <- c(0, 3, 0, 1, -3)
-current <- 1
-while (TRUE) {
-  length_steps <- length(steps)
-  next_up <- current + steps[current]
-  steps[current] <- steps[current] + 1
-  if (next_up > length_steps || next_up < 1) {
-    print(steps)
-    break
-  } else {
-    current <- next_up
+step_it <- function(steps) {
+  steps_taken <- 0L
+  current <- 1L
+  while (TRUE) {
+    length_steps <- length(steps)
+    next_up <- current + steps[current]
+    steps[current] <- steps[current] + 1L
+    steps_taken <- steps_taken + 1L
+    if (next_up > length_steps || next_up < 1) {
+      break
+    } else {
+      current <- next_up
+    }
   }
+  return(steps_taken)
 }
+step_it(c(0, 3, 0, 1, -3))
