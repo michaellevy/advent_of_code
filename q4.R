0883fef2cacab294b38b838b83a2c73bb66d2723
Michael Levy <michael.levy@healthcatalyst.com>
2018-02-04 18:43:46 -0700

    q4 part 1 checked

--- q4.R
+++ q4.R
@@ -1,9 +1,10 @@
-phrases <- list("i am not a robot.", "i do have a repeat i do", "no not here")
-
 library(tidyverse)
 library(stringr)
 is_valid <- function(phrase) {
   word_list <- str_split(phrase, fixed(" ")) %>% flatten()
   !any(map_lgl(seq_along(word_list), ~ any(word_list[[.x]] %in% word_list[-.x])))
 }
-map_lgl(phrases, is_valid)
+
+read_lines("inputs/q4.txt") %>%
+  map_lgl(is_valid) %>%
+  sum()
