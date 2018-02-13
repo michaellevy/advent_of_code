32b3fd2b6a9c21935b6063a8a567d020e41e7959
Michael Levy <michael.levy@healthcatalyst.com>
2018-02-12 19:27:05 -0700

    q4 part 2

--- q4.R
+++ q4.R
@@ -2,9 +2,25 @@ library(tidyverse)
 library(stringr)
 is_valid <- function(phrase) {
   word_list <- str_split(phrase, fixed(" ")) %>% flatten()
-  !any(map_lgl(seq_along(word_list), ~ any(word_list[[.x]] %in% word_list[-.x])))
+  length(word_list) == length(unique(word_list))
 }
 
 read_lines("inputs/q4.txt") %>%
   map_lgl(is_valid) %>%
+  sum()
+
+is_valid2 <- function(phrase) {
+  word_list <-
+    str_split(phrase, fixed(" ")) %>%
+    flatten() %>%
+    map_chr(~ {
+      str_split(.x, "")[[1]] %>%
+        sort() %>%
+        paste(collapse = "")
+      })
+  length(word_list) == length(unique(word_list))
+}
+
+read_lines("inputs/q4.txt") %>%
+  map_lgl(is_valid2) %>%
   sum()
