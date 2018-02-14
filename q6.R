8587026fa22fdc21edd4525b8a66b78b598b1b4b
Michael Levy <michael.levy@healthcatalyst.com>
2018-02-13 22:11:16 -0700

    q6 part 2

--- q6.R
+++ q6.R
@@ -32,3 +32,24 @@ redist <- function(state, i = which.max(state), n = length(state)) {
 }
 
 go_init(init, 1e4)
+
+go_loop <- function(state, len) {
+  i <- 1L
+  found <- FALSE
+  states <- character(len)
+  stopper = "aaa"
+  while (TRUE) {
+    state <- redist(state)
+    char_state <- paste(state, collapse = "")
+    if (!found && char_state %in% states) {  # First time
+      stopper <- char_state
+      found <- TRUE
+      t1 <- i
+    } else if (char_state == stopper) {
+      return(i - t1)
+    }
+    states[[i]] <- char_state
+    i <- i + 1L
+  }
+}
+go_loop(init, 1e4)
