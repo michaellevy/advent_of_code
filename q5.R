85f4c04471749c467f8ca6112fa45d4568ac1e0e
Michael Levy <michael.levy@healthcatalyst.com>
2018-02-13 20:27:26 -0700

    q5 part 2

--- q5.R
+++ q5.R
@@ -1,8 +1,9 @@
+library(tidyverse)
 step_it <- function(steps) {
   steps_taken <- 0L
   current <- 1L
+  length_steps <- length(steps)
   while (TRUE) {
-    length_steps <- length(steps)
     next_up <- current + steps[current]
     steps[current] <- steps[current] + 1L
     steps_taken <- steps_taken + 1L
@@ -17,3 +18,24 @@ step_it <- function(steps) {
 read_lines("inputs/q5.txt") %>%
   as.integer() %>%
   step_it()
+
+step_it2 <- function(steps) {
+  steps_taken <- 0L
+  current <- 1L
+  length_steps <- length(steps)
+  while (TRUE) {
+    next_up <- current + steps[current]
+    steps[current] <- steps[current] +
+      if (steps[current] >= 3) -1L else 1L
+    steps_taken <- steps_taken + 1L
+    if (next_up > length_steps || next_up < 1) {
+      break
+    } else {
+      current <- next_up
+    }
+  }
+  return(steps_taken)
+}
+read_lines("inputs/q5.txt") %>%
+  as.integer() %>%
+  step_it2()
