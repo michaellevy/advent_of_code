f30eec49c4c4fa4f05d2a50a7b6b9221a48411fe
Michael Levy <michael.levy@healthcatalyst.com>
2018-02-15 06:42:00 -0700

    q3 part 2

--- q3.R
+++ q3.R
@@ -13,3 +13,37 @@ steps_to_center <- function(input) {
 }
 
 steps_to_center(312051)
+
+# Part 2
+
+## Try this manually, with n as the center point of the grid
+get_dir <- function(m = m, pos = pos, dirs = dirs, dir = dir) {
+  if (dir == "r") {
+    if (is.na(m[pos + dirs$u])) "u" else "r"
+  } else if (dir == "u") {
+    if (is.na(m[pos + dirs$l])) "l" else "u"
+  } else if (dir == "l") {
+    if (is.na(m[pos + dirs$d])) "d" else "l"
+  } else {
+    if (is.na(m[pos + dirs$r])) "r" else "d"
+  }
+}
+
+n = 11
+m <- matrix(NA_integer_, nrow = n * 2 - 1, ncol = n * 2 - 1)
+pos <- cbind(n, n)
+m[pos] <- 1L
+dirs <- list(d = cbind(1, 0), u = cbind(-1, 0), l = cbind(0, -1), r = cbind(0, 1))
+dir <- "r"
+
+prev <- 1L
+while(prev < 312051) {
+  if (is.na(m[pos])) {
+    m[pos] <- sum(m[as.matrix(expand.grid(x = pos[, 1] + -1:1, y = pos[, 2] + -1:1))], na.rm = TRUE)
+    dir <- get_dir(m, pos, dirs, dir)
+  }
+  prev <- m[pos]
+  pos <- pos + dirs[[dir]]
+}
+m
+prev
